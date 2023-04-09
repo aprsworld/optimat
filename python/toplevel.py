c72863ce55bdaa7fddaef26f0f4d78fa264bf163
James Jarvis <jj@aprsworld.com>
2023-04-09 15:10:58 -0500

    Looks like I can remap

---
+++ python/toplevel.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+print "########## starting toplevel.py ###################" 
+
+import sys
+# sys.path.append('/usr/share/linuxcnc/ncfiles/remap_lib/python-stdglue')
+
+
+print "######### sys.path now:"
+print sys.path
+
+# print "######### importing everything from stdglue:"
+# from stdglue import *
+
+print "######### done with toplevel.py"
+import remap
+
+
